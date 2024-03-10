cb8c244792bedca712da3e3cc00cb0a57537a084
Rayhan Alfa Rezki <125686480+rayhanrezki@users.noreply.github.com>
2024-03-10 19:51:31 +0700

    tugas pertemuan 5

---
+++ pertemuan5/Latihan2RemoveHead.java
@@ -0,0 +1,23 @@
+package pertemuan5;
+
+public class Latihan2RemoveHead {
+
+	
+	public static void main(String[] args) {
+        StrukturList list = new StrukturList();
+
+        // Tambah elemen 2 di awal list
+        list.addHead(7);
+        // Tambah elemen 9 di awal list
+        list.addMid(9, 0);
+        // Tambah elemen 7 di awal list
+        list.addTail(2);
+      
+	    
+        // Tampilkan elemen list
+        list.displayElement(); // Output: 7 9 2
+        
+
+       
+    }
+}
